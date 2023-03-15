fa193cacce84862db697e7bb6f86514e000e437b
sourav mete <souravjan28@gmail.com>
2023-03-15 18:21:01 +0530

    first commit of portfolio site

---
+++ index.js
@@ -0,0 +1,13 @@
+// auto input script 
+var typed = new Typed(".auto-input",{
+    strings:["Web Developer","Frontend Developer","UI Designer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+
+
+// navbar active link 
+
